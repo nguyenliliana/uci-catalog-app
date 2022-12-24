2156f46a519244b6e0d1834477dfe523e6d697d5
nguyenliliana <liliananguyen@ucsb.edu>
2022-12-23 21:09:23 -0800

    added more info and styling

--- src/components/Course.js
+++ src/components/Course.js
@@ -7,14 +7,24 @@ function Course({post}) {
     return sections.map(function (section){
       if (section.sectionType == "Lec") {
         return (
-        <div>
-          {section.instructors.map(i => <p>{section.sectionType}: {i}</p>)}
+        <div className="lecture">
+          <div className="lecture-instructors">
+            <p> {section.sectionType}: </p>
+            {section.instructors.map(i => <p> {i}</p>)}
+          </div>
+          <div className="section-info">
+            <p className="section-code">   {section.sectionCode} </p>
+            <p className="section-meeting"> {section.meetings[0].days} {section.meetings[0].time} </p>
+            <p className="section-location"> {section.meetings[0].bldg} </p>
+            <p className="section-capacity"> {section.numCurrentlyEnrolled.totalEnrolled}/{section.maxCapacity}</p>
+          </div>
         </div>
       )
       } else {
         return (
-          <div>
+          <div className="discussion">
             {section.instructors.map(i => <p>{section.sectionType}: {i}</p>)}
+            
           </div>
         )
       }
@@ -24,7 +34,7 @@ function Course({post}) {
   return (
     <div key="{post.courseNumber}" className="courseInfo">
         <p>Course #/Title: {post.courseNumber}/{post.courseTitle}</p>
-        <p>{getSections(post.sections)}</p>
+        <p className="sections">{getSections(post.sections)}</p>
     </div>
   )
 }
